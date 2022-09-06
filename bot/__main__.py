536a807d3fa7b6a97e8038808050af572c74c852
Saemfvck <44951380+saemfvck@users.noreply.github.com>
2022-09-06 10:37:48 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -46,8 +46,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Repo", "https://www.github.com/saemfvck/mirrorbot")
-    buttons.buildbutton("Owner", "https://t.me/MathiasFelice")
+    buttons.buildbutton("Repo", "https://github.com/anasty17/mirror-leech-telegram-bot
+    buttons.buildbutton("Owner", "https://www.github.com/anasty17")
     reply_markup = buttons.build_menu(2)
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
